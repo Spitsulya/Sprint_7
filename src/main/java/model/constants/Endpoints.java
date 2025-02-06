e84be807aebd862a400dcf9ede93531deac602a2
Spitsulya <spitsulya15@yandex.ru>
2025-02-06 22:42:37 +0300

    Moved endpoints and url to a separate classes

---
+++ src/main/java/model/constants/Endpoints.java
@@ -0,0 +1,9 @@
+package model.constants;
+
+public class Endpoints {
+    public static final String CREATE_COURIER = "/api/v1/courier";
+    public static final String LOGIN_COURIER = "/api/v1/courier/login";
+    public static final String DELETE_COURIER = "/api/v1/courier/";
+    public static final String CREATE_ORDER = "/api/v1/orders";
+    public static final String CANCEL_ORDER = "/api/v1/orders/cancel";
+}
