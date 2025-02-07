60f3d167364174cb1724ba9b93d71c99d2329bc3
Spitsulya <spitsulya15@yandex.ru>
2025-02-07 16:24:47 +0300

    Code readability edits

--- src/main/java/client/ScooterServiceClient.java
+++ src/main/java/client/ScooterServiceClient.java
@@ -1,30 +1,34 @@
 package client;
+import io.qameta.allure.restassured.AllureRestAssured;
 import io.restassured.response.ValidatableResponse;
 import model.CourierData;
 import io.qameta.allure.Step;
 import model.Credentials;
 import static io.restassured.RestAssured.given;
+import model.OrderRequest;
+import model.constants.*;
 
 
 public class ScooterServiceClient {
 
     private String baseURI;
 
-    public ScooterServiceClient(String baseURI) {
-        this.baseURI = baseURI;
+    public ScooterServiceClient() {
+        this.baseURI = Url.BASE_URI;
     }
 
     @Step("Courier creating with data")
     public ValidatableResponse createCourierPostRequest(CourierData courierData) {
 
         return given()
+                        .filter(new AllureRestAssured())
                         .log()
                         .all()
                         .baseUri(baseURI)
                         .header("Content-type", "application/json")
                         .body(courierData)
                         .when()
-                        .post("/api/v1/courier")
+                        .post(Endpoints.CREATE_COURIER)
                         .then()
                         .log()
                         .all();
@@ -33,12 +37,13 @@ public class ScooterServiceClient {
     @Step("Courier authorization with existing data")
     public ValidatableResponse authorizeCourier(Credentials credentials) {
         return given()
+                .filter(new AllureRestAssured())
                 .log()
                 .all()
                 .baseUri(baseURI)
                 .header("Content-type", "application/json")
                 .body(credentials)
-                .post("/api/v1/courier/login")
+                .post(Endpoints.LOGIN_COURIER)
                 .then()
                 .log()
                 .all();
@@ -48,15 +53,68 @@ public class ScooterServiceClient {
     public ValidatableResponse deleteCourier(int courierId) {
 
         return given()
+                .filter(new AllureRestAssured())
                 .log()
                 .all()
                 .baseUri(baseURI)
                 .header("Content-type", "application/json")
                 .when()
-                .delete("/api/v1/courier/" + courierId)
+                .delete(Endpoints.DELETE_COURIER + courierId)
                 .then()
                 .log()
                 .all()
                 .statusCode(200);
+    }
+
+    @Step("POST request to \"/api/v1/orders\" to create an order")
+    public ValidatableResponse sendCreateOrderRequest(OrderRequest orderRequest) {
+
+        return given()
+                        .filter(new AllureRestAssured())
+                        .log()
+                        .all()
+                        .baseUri(baseURI)
+                        .header("Content-type", "application/json")
+                        .body(orderRequest)
+                        .post(Endpoints.CREATE_ORDER)
+                        .then()
+                        .log()
+                        .all();
+    }
+
+    @Step("Extract track number from response")
+    public int extractTrackNumber(ValidatableResponse response) {
+        return response.extract().path("track");
+    }
+
+    @Step("Order canceling and completing the test, PUT /api/v1/orders/cancel/ with params track")
+    public ValidatableResponse cancelOrder(int orderTrack) {
+        return given()
+                .filter(new AllureRestAssured())
+                .log()
+                .all()
+                .baseUri(baseURI)
+                .header("Content-type", "application/json")
+                .when()
+                .queryParam("track", orderTrack)
+                .put(Endpoints.CANCEL_ORDER)
+                .then()
+                .log()
+                .all()
+                .statusCode(200);
+    }
+
+    @Step("Send GET request to retrieve the order list")
+    public ValidatableResponse getOrderList() {
+        return given()
+                .filter(new AllureRestAssured())
+                .log()
+                .all()
+                .baseUri(baseURI)
+                .header("Content-type", "application/json")
+                .get(Endpoints.GET_ORDER_LIST)
+                .then()
+                .log()
+                .all();
     }
 }
--- src/main/java/model/constants/Endpoints.java
+++ src/main/java/model/constants/Endpoints.java
@@ -6,4 +6,5 @@ public class Endpoints {
     public static final String DELETE_COURIER = "/api/v1/courier/";
     public static final String CREATE_ORDER = "/api/v1/orders";
     public static final String CANCEL_ORDER = "/api/v1/orders/cancel";
+    public static final String GET_ORDER_LIST = "/api/v1/orders";
 }
