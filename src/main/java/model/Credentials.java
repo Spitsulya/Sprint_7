a950862cf77934544606316a6f34e71620a36ee5
Spitsulya <spitsulya15@yandex.ru>
2025-02-08 20:57:52 +0300

    Shortened the code by implementing the Lombok class

--- src/main/java/model/Credentials.java
+++ src/main/java/model/Credentials.java
@@ -1,13 +1,13 @@
 package model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class Credentials {
     private String login;
     private String password;
-
-    public Credentials(String login, String password) {
-        this.login = login;
-        this.password = password;
-    }
 
     // корректные данные для авторизации
     public static Credentials fromCourierData(CourierData courierData) {
@@ -27,22 +27,5 @@ public class Credentials {
     // передаем несуществующие данные курьера
     public static Credentials invalidData(CourierData courierData) {
         return new Credentials("invalidLogin", "invalidPassword");
-    }
-
-
-    public String getLogin() {
-        return login;
-    }
-
-    public void setLogin(String login) {
-        this.login = login;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
     }
 }
--- src/main/java/model/OrderData.java
+++ src/main/java/model/OrderData.java
@@ -1,32 +1,12 @@
 package model;
+import lombok.*;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class OrderData {
     private String id;
     private String courierId;
-
-    public OrderData(String id, String courierId) {
-        this.id = id;
-        this.courierId = courierId;
-    }
-
-    public OrderData() {
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getCourierId() {
-        return courierId;
-    }
-
-    public void setCourierId(String courierId) {
-        this.courierId = courierId;
-    }
 }
